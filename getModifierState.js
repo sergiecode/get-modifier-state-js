45e4fb902e4a59d5d019dd4b0d0f811ce4608633
SnSDk <sergiosnsdk@gmail.com>
2023-08-03 10:31:22 -0300

    get modifier state

---
+++ getModifierState.js
@@ -0,0 +1,13 @@
+
+
+
+document.addEventListener('keydown', function(event) {
+    if (event.getModifierState('CapsLock')) {
+      // La tecla CapsLock está activada
+    } else {
+      // La tecla CapsLock está desactivada
+    }
+  });
+  
+
+  
